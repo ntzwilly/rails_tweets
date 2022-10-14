f786cc3606451aced0b3f79e264b945f0f32e5c9
ntzwilly <ntzwilly@gmail.com>
2022-10-14 23:06:34 +0200

    Implement sign_in and sign_out

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   get "about-us", to: "about#index", as: :about
+
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+  delete "logout", to: "sessions#destroy"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+  
+  
   root to: "main#index"
 end
